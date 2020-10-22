303fd7c353f3ec225e5aacd81508106e8e9fcb1b
M S Saurabh <magicalme049@gmail.com>
2020-10-22 18:52:01 -0500

    Added Logistic regression; Calculated FPR by race

--- compas_explore.py
+++ compas_explore.py
@@ -6,6 +6,9 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+import matplotlib.pyplot as plt
 
 PATH_COMPAS_VIOLENT = os.path.join(os.getcwd(), 'data', 'compas-scores-two-years-violent.csv')
 
@@ -40,45 +43,88 @@ df = preprocess_compas(df, usecols)
 X = df.drop(TARGET_NAME, axis=1)
 y = df[TARGET_NAME]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42, stratify=y)
 
-tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000], 'C': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000]},
-                    {'kernel': ['linear'], 'C': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000]}]
+def explore_algorithm(algo, tuned_parameters, scores = ['accuracy']):
+    for score in scores:
+        print("# Tuning hyper-parameters for %s" % score)
+        print()
 
-scores = ['accuracy']
+        clf = GridSearchCV(
+            algo(), tuned_parameters, scoring=score, verbose=1, n_jobs=5, cv=3
+        )
+        clf.fit(X_train, y_train)
 
-for score in scores:
-    print("# Tuning hyper-parameters for %s" % score)
-    print()
+        print("Best parameters set found on development set:")
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r"
+                % (mean, std * 2, params))
+        print()
 
-    clf = GridSearchCV(
-        SVC(), tuned_parameters, scoring=score, verbose=1, n_jobs=5, cv=3
-    )
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+def plot_fpr(columns, fpr_list, clf_name):
+    plt.style.use('ggplot')
+
+    x_pos = [i for i, _ in enumerate(columns)]
+
+    plt.bar(x_pos, fpr_list, color='green')
+    plt.xlabel("Race")
+    plt.ylabel("False Positive Rate")
+    plt.title("FPR variation by race")
+
+    plt.xticks(x_pos, columns)
+
+    plt.savefig("figures/{}_fpr_plot.png".format(clf_name))
+
+def fp_by_class(X_test, y_test, y_pred, columns):
+    fpr_list = []
+    sample_counts = []
+    for val in columns:
+        # For each class
+        class_indices = (X_test[val] == 1)
+        y_test_col = y_test[class_indices]
+        y_pred_col = y_pred[class_indices]
+        fp = np.count_nonzero((y_pred_col == 1)  & (y_test_col == 0))
+        tn = np.count_nonzero(y_test_col == 0)
+        fpr = fp / tn
+        fpr_list.append(fpr)
+        sample_counts.append(len(y_pred_col))
+    return fpr_list, sample_counts
+
+def fpr_by_group(classifier, params):
+    clf = classifier(**params)
     clf.fit(X_train, y_train)
+    print(clf)
+    y_pred = clf.predict(X_test)
+    columns = ['African-American', 'Asian', 'Caucasian', 'Hispanic', 'Native American', 'Other']
+    fpr_list, sample_counts = fp_by_class(X_test, y_test, y_pred, columns)
 
-    print("Best parameters set found on development set:")
-    print()
-    print(clf.best_params_)
-    print()
-    print("Grid scores on development set:")
-    print()
-    means = clf.cv_results_['mean_test_score']
-    stds = clf.cv_results_['std_test_score']
-    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-        print("%0.3f (+/-%0.03f) for %r"
-              % (mean, std * 2, params))
-    print()
+    for col, count, fpr in zip(columns, sample_counts, fpr_list):
+        print("{}:{:.2f} in {} samples".format(col, fpr, count))
+    plot_fpr(columns, fpr_list, classifier.__name__)
 
-    print("Detailed classification report:")
-    print()
-    print("The model is trained on the full development set.")
-    print("The scores are computed on the full evaluation set.")
-    print()
-    y_true, y_pred = y_test, clf.predict(X_test)
-    print(classification_report(y_true, y_pred))
-    print()
+fpr_by_group(LogisticRegression, {'C':100})
+fpr_by_group(SVC, {'C': 1, 'gamma': 0.1, 'kernel': 'rbf'})
 
-# data = np.genfromtxt(PATH_COMPAS_VIOLENT, delimiter=',', names=True, usecols=usecols, dtype=types)
 
-# print("Violent same as is_recid?", np.count_nonzero(data['is_violent_recid'] != data['is_recid']))
-# print("2-year same as is_recid?", np.count_nonzero(data['two_year_recid'] != data['is_recid']))
+# SVM_parameters = [{'kernel': ['rbf'], 'gamma': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000], 'C': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000]},
+#                         {'kernel': ['linear'], 'C': [1e-3, 1e-2, 0.1, 1, 10, 100, 1000]}]
+# # explore_algorithm(SVC, SVM_parameters)
+
+# LR_parameters = [{'C':[1e-3, 1e-2, 0.1, 1, 10, 100, 1000]}]
+# explore_algorithm(LogisticRegression, LR_parameters)
