de5036ac89fb818d4634c53d42a0e076a390002f
M S Saurabh <magicalme049@gmail.com>
2020-12-16 21:47:26 +0530

    Reweighing added

--- common_utils.py
+++ common_utils.py
@@ -2,6 +2,8 @@
 from collections import OrderedDict
 from aif360.metrics import ClassificationMetric
 
+import matplotlib.pyplot as plt
+
 def compute_metrics(dataset_true, dataset_pred, 
                     unprivileged_groups, privileged_groups,
                     disp = True):
@@ -24,3 +26,50 @@ def compute_metrics(dataset_true, dataset_pred,
             print("%s = %.4f" % (k, metrics[k]))
     
     return metrics
+
+def plot_metrics(metrics_before, metrics_after, 
+                 sensitive_attr='Race',
+                 mitigation='Adversarial De-biasing', dataset='Compas'):
+    total = 4; cols = 4
+    rows = total //cols; rows += total % cols
+    pos = range(1, total+1)
+    
+    # Plot styles
+    plt.style.use('ggplot')
+    options = {'font.size': 18,
+           'figure.titlesize': 30}
+    plt.rcParams.update(options)
+    
+    fig = plt.figure(figsize=(36,rows*8))
+    x_pos = [1,2]
+    kwargs = {
+        'color': ['grey', 'teal'],
+        'tick_label': ['Before mitigation', 'After mitigation']
+    }
+    
+    axes_background = (0.9,0.9,0.9)
+    i = 0; subplot_pad=20
+    ax = fig.add_subplot(rows, cols, pos[i])
+    ax.set_title('Classification Accuracy Difference', pad=subplot_pad)
+    ax.bar(x_pos, [metrics_before.accuracy(), metrics_after.accuracy()], **kwargs)
+
+    metrics = {'Demographic Parity Difference': 'mean_difference',
+               'Disparate Impact': 'disparate_impact',
+               'Equal Opportunity Difference': 'equal_opportunity_difference'}
+    
+    for title, methodname in  metrics.items():
+        i += 1;
+        ax = fig.add_subplot(rows, cols, pos[i])
+        ax.yaxis.grid()
+        ax.set_title(title, pad=subplot_pad)
+        ax.set_ylim([-0.5,1])
+        ax.axhline(0, color='k')
+        ax.bar(x_pos, 
+               [getattr(metrics_before, methodname)(), getattr(metrics_after, methodname)()],
+               **kwargs)
+
+    fig.subplots_adjust(bottom=0.2)
+    title = 'Impact of {} on {} dataset - {}'.format(mitigation, dataset, sensitive_attr)
+    fig.suptitle(title, y=0.08)
+    fig.savefig('figures/'+title+'.png')
+    plt.show()
